49f88e932087e472e5b555318da213e0118fdf16
Chatlada-Nina <chatladanine@gmail.com>
2025-02-13 20:33:42 +0100

    Test webhooks via stripeCLI

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-^*r+=_)yq&@gazlsc$xk-s2m9!ra6ugtdpkiorq=pax%694ty3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', ]
 
 
 # Application definition

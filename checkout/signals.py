1b35ad9c30daf71aa44887116832e65a982ea712
Chatlada-Nina <chatladanine@gmail.com>
2025-02-13 13:15:59 +0100

    Add checkout submit logic

--- checkout/signals.py
+++ checkout/signals.py
@@ -5,7 +5,7 @@ from .models import OrderLineItem
 
 
 @receiver(post_save, sender=OrderLineItem)
-def update_on_save(sender, instance, create, **kwargs):
+def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem update/create
     """
@@ -13,8 +13,9 @@ def update_on_save(sender, instance, create, **kwargs):
 
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
+    print('delete signal received!')
     instance.order.update_total()

ecc7e8eb8d4770604b5e850c0b3f01ceb93bff59
Chatlada-Nina <chatladanine@gmail.com>
2025-02-14 12:25:50 +0100

    Update checkout form

--- checkout/models.py
+++ checkout/models.py
@@ -4,15 +4,17 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
+from django_countries.fields import CountryField
+
 from products.models import Product
 
-# Create your models here.
+
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label="Country *", null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -25,16 +27,16 @@ class Order(models.Model):
     original_bag = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
-
     def _generate_order_number(self):
         """
-        Generate a random, unigue order number using UUID
+        Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
- 
+
     def update_total(self):
         """
-        Update grand total each time a line item is added, accounting for delivery costs.
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
@@ -44,10 +46,10 @@ class Order(models.Model):
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
- 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the order number if it hasn't been set already.
+        Override the original save method to set the order number
+        if it hasn't been set already.
         """
         if not self.order_number:
             self.order_number = self._generate_order_number()
@@ -66,11 +68,11 @@ class OrderLineItem(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the set the lineitem total and update the order total.
+        Override the original save method to set the lineitem total
+        and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
-
